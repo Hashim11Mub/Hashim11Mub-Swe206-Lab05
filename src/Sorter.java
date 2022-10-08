a266f469a30fbc3974867f6490604c66a51ceb45
AMHM2015 <amhm.05.2011@gmail.com>
2022-10-08 19:36:04 +0300

    3rd commit

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,21 @@
-public class App {
+public class Sorter {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+    public static void  insertionSort(int[]arr){
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+  
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
 }
