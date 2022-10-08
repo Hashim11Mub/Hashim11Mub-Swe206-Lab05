c19a3f3f1e13238fbe7fb07781c04f6117d6ffcb
AMHM2015 <amhm.05.2011@gmail.com>
2022-10-08 20:05:06 +0300

    final

--- src/Sorter.java
+++ src/Sorter.java
@@ -18,7 +18,7 @@ public class Sorter {
 
     }
 
-    private static void sort(int[] arr1) {
+    public static void sort(int[] arr1) {
         selectionSort(arr1);
     }
 
@@ -35,5 +35,22 @@ public class Sorter {
             arr[i] = smallerNumber;
         }
     }
+    public static void insertionSort(int[] arr){
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+  
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    
    
 }
